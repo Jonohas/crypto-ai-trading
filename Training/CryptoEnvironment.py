ebe3d894362e6fc79d1c9f69e2158b1c4ed5a97a
Jonas Faber <jonas.h.faber@gmail.com>
2023-01-13 12:38:45 +0100

    feat: add reset functionality to environment

--- Training/CryptoEnvironment.py
+++ Training/CryptoEnvironment.py
@@ -11,6 +11,8 @@ class CryptoEnvironment(gym.Env):
     def __init__(self, data, arguments, verbose=False):
         super(CryptoEnvironment, self).__init__()
 
+        self._default_arguments = arguments
+
         self._verbose = verbose
 
         self._data = data
@@ -19,18 +21,18 @@ class CryptoEnvironment(gym.Env):
 
         self._input_shape = ()
 
-        self._look_back_window = arguments["look_back_window"]
-        self._look_ahead_window = arguments["look_ahead_window"]
+        self._look_back_window = self._default_arguments["look_back_window"]
+        self._look_ahead_window = self._default_arguments["look_ahead_window"]
 
-        self._tick = 0 # step count
+        self._tick = self._default_arguments["look_back_window"] - 1 # step count
 
         self._action_space = spaces.Discrete(len(ActionSpace))
         self._observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=self._input_shape, dtype=np.double)
 
-        self._current_candle = None
+        self._current_candle = self._get_state(self._tick)
 
-        self._account_balance = arguments["initial_balance"]
-        self._coin_amount = arguments["initial_coin_amount"]
+        self._account_balance = self._default_arguments["initial_balance"]
+        self._coin_amount = self._default_arguments["initial_coin_amount"]
         self._position = Positions.CLOSED
 
         self._previous_sell_tick = 0
@@ -38,12 +40,28 @@ class CryptoEnvironment(gym.Env):
 
         self._total_profit = 0
         
-        self._profitable_sell_threshold = arguments["profitable_sell_threshhold"] # 1% profit
-        self._profitable_sell_reward = arguments["profitable_sell_reward"]
+        self._profitable_sell_threshold = self._default_arguments["profitable_sell_threshhold"] # 1% profit
+        self._profitable_sell_reward = self._default_arguments["profitable_sell_reward"]
         self._none_profitable_sell_reward = -self._profitable_sell_reward
 
+    def _get_state(self, tick):
+        return self._data.iloc[tick]
+
     def reset(self):
-        pass
+        self._tick = self._default_arguments["look_back_window"] - 1
+        self._current_candle = self._get_state(self._tick)
+
+        self._account_balance = self._default_arguments["initial_balance"]
+        self._coin_amount = self._default_arguments["initial_coin_amount"]
+
+        self._profitable_sell_threshold = self._default_arguments["profitable_sell_threshhold"] # 1% profit
+        self._profitable_sell_reward = self._default_arguments["profitable_sell_reward"]
+        self._none_profitable_sell_reward = -self._profitable_sell_reward
+
+        self._total_profit = 0
+        self._previous_buy_tick = 0
+        self._previous_sell_tick = 0
+        return self._current_candle
 
     def step(self, action):
         pass
--- Training/train.py
+++ Training/train.py
@@ -13,9 +13,28 @@ from CryptoEnvironment import CryptoEnvironment
 load_dotenv()
 
 class Train():
-    def __init__(self, data_path, arguments, verbose=False):
-        self._verbose = verbose
+    def __init__(self, data_path):
         self._data = pd.read_csv(data_path)
+
+        env_risk = float(os.getenv("ENVIRONMENT_RISK_FACTOR"))
+        env_initial_balance = float(os.getenv("ENVIRONMENT_INITIAL_BALANCE"))
+        env_initial_coin_amount = float(os.getenv("ENVIRONMENT_INITIAL_COIN_AMOUNT"))
+        env_look_back_window = int(os.getenv("ENVIRONMENT_LOOK_BACK_WINDOW"))
+        env_look_ahead_window = int(os.getenv("ENVIRONMENT_LOOK_AHEAD_WINDOW"))
+        env_profitable_sell_threshhold = float(os.getenv("ENVIRONMENT_PROFITABLE_SELL_THRESHHOLD"))
+        env_profitable_sell_reward = float(os.getenv("ENVIRONMENT_PROFITABLE_SELL_REWARD"))
+
+        self._verbose = bool(os.getenv("VERBOSE"))
+
+        arguments={
+            'risk':  env_risk,
+            'initial_balance':  env_initial_balance,
+            'initial_coin_amount':  env_initial_coin_amount,
+            'look_back_window':  env_look_back_window,
+            'look_ahead_window':  env_look_ahead_window,
+            'profitable_sell_threshhold':  env_profitable_sell_threshhold,
+            'profitable_sell_reward':  env_profitable_sell_reward
+        }
 
         self._env = CryptoEnvironment(self._data, arguments=arguments, verbose=self._verbose)
 
@@ -26,23 +45,7 @@ class Train():
 if __name__ == "__main__":
     data_path = os.getenv("DATA_PATH")
 
-    env_risk = float(os.getenv("ENVIRONMENT_RISK_FACTOR"))
-    env_initial_balance = float(os.getenv("ENVIRONMENT_INITIAL_BALANCE"))
-    env_initial_coin_amount = float(os.getenv("ENVIRONMENT_INITIAL_COIN_AMOUNT"))
-    env_look_back_window = int(os.getenv("ENVIRONMENT_LOOK_BACK_WINDOW"))
-    env_look_ahead_window = int(os.getenv("ENVIRONMENT_LOOK_AHEAD_WINDOW"))
-    env_profitable_sell_threshhold = float(os.getenv("ENVIRONMENT_PROFITABLE_SELL_THRESHHOLD"))
-    env_profitable_sell_reward = float(os.getenv("ENVIRONMENT_PROFITABLE_SELL_REWARD"))
-
-    _verbose = bool(os.getenv("VERBOSE"))
 
 
-    train = Train(data_path, verbose=_verbose, arguments={
-        'risk':  env_risk,
-        'initial_balance':  env_initial_balance,
-        'initial_coin_amount':  env_initial_coin_amount,
-        'look_back_window':  env_look_back_window,
-        'look_ahead_window':  env_look_ahead_window,
-        'profitable_sell_threshhold':  env_profitable_sell_threshhold,
-        'profitable_sell_reward':  env_profitable_sell_reward
-    })
+
+    train = Train(data_path)
